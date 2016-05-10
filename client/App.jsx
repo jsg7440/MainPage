d3421652f2d266f6a1e8f41fc31b57b7db4def46
Jay G <jsg7440@gmail.com>
2016-05-09 20:17:51 -0600

    updating again

--- client/App.jsx
+++ client/App.jsx
@@ -1,2 +1,10 @@
+import $ from 'jquery';
 import 'styles/main.scss';
-import 'pages/todo';
+import todos from 'pages/todo';
+import project from 'pages/project';
+
+todos.init();
+
+$(function(){
+	todos.init();
+});
