e4e00afe5ba0c797558578ac79eadfc100da8391
Jay G <jsg7440@gmail.com>
2016-06-20 20:30:07 -0600

    Rebuilding Todo in React

--- client/App.jsx
+++ client/App.jsx
@@ -2,10 +2,11 @@
 import $ from 'jquery';
 import 'styles/main.scss';
 import TodoControllerView from 'pages/todo/todoController';
-// import project from 'pages/project';
+import project from 'pages/project';
 import funnySquares from 'pages/funnySquares';
 import header from 'components/header';
 import photoSearch from 'pages/photoSearch';
+import TodoReactControllerView from 'pages/todoReact/todoController';
 
 $(function(){
 
@@ -18,7 +19,10 @@ $(function(){
     case '/pages/todo.html':
       var todoControllerView = new TodoControllerView();
     break;
-    // case '/':
+    case '/pages/todoReact.html':
+      var todoReactControllerView = new TodoReactControllerView();
+    break;
+    // case '/pages/project.html':
     //   // init the project javascript
     //   home.init();
     // break;
